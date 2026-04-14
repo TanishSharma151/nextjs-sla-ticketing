07220d8ddd208ad0a949b23258e8806600d6fb2d
Tanish Raj Sharma <tanishrajsharma151@gmail.com>
2026-04-14 18:04:12 +0530

    Update email.ts

--- app/api/lib/email.ts
+++ app/api/lib/email.ts
@@ -1,7 +1,5 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function sendEmail({
   to,
   subject,
@@ -12,12 +10,20 @@ export async function sendEmail({
   text: string;
 }) {
   try {
+    if (!process.env.RESEND_API_KEY) {
+      console.error("Missing RESEND_API_KEY");
+      return;
+    }
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
     await resend.emails.send({
       from: "onboarding@resend.dev",
       to,
       subject,
       text,
     });
+
   } catch (err) {
     console.error("Email error:", err);
   }
