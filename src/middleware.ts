d8540976bc97f9e67f4636c229acacc651d6f56f
TanishSharma151 <tanishrajsharma151@gmail.com>
2026-06-08 19:35:23 +0530

    Production ready application

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,6 @@
 import { NextResponse } from 'next/server';
 
-import type {
-  NextRequest,
-} from 'next/server';
-
-export function middleware(
-  request: NextRequest,
-) {
-  const token =
-    request.cookies.get(
-      'access_token',
-    );
-
-  const isAuthPage =
-    request.nextUrl.pathname ===
-      '/login'
-    ||
-    request.nextUrl.pathname ===
-      '/signup';
-
-  if (!token && !isAuthPage) {
-    return NextResponse.redirect(
-      new URL(
-        '/login',
-        request.url,
-      ),
-    );
-  }
-
+export function middleware() {
   return NextResponse.next();
 }
 
